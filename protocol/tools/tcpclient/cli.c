1e5961b82616465d416309cb3841748b2ab1c5f9
ivanallen <1007729991@qq.com>
2017-04-06 16:00:00 +0800

    添加 tcp client

---
+++ protocol/tools/tcpclient/cli.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define ERR_EXIT(msg) do { perror(msg); exit(1); } while(0)
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) return 1;
+  int sockfd, ret, n;
+  char buf[64];
+  struct sockaddr_in servaddr;
+  struct sockaddr_in cliaddr;
+  socklen_t cliaddrlen;
+  
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+  servaddr.sin_port = htons(atoi(argv[2]));
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) ERR_EXIT("socket");
+
+  ret = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+  if (ret < 0) ERR_EXIT("connect");
+
+
+  while(1) {
+    scanf("%s", buf);
+    if (buf[0] == 'q') break;
+    write(sockfd, buf, strlen(buf));
+    n = read(sockfd, buf, 63);
+    buf[n] = 0;
+    puts(buf);
+  }
+
+  close(sockfd);
+  return 0;
+}
+
