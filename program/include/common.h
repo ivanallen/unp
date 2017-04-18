9387418dba0ddb5f127d8e25ce779d542da0f2aa
allen <1007729991@qq.com>
2017-04-18 19:24:59 +0800

    add basic echo

---
+++ program/include/common.h
@@ -0,0 +1,59 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <map>
+#include <string>
+#include <assert.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/epoll.h>
+
+
+#define ERR_EXIT(msg) do { perror(msg); exit(1); } while(0)
+#define CONTAINS(container,element) (container.find(element) != container.end())
+#define SETINT(args,val,opt,def) \
+  do {\
+    if (CONTAINS(args,opt)) {\
+      (val) = atoi(args[opt].c_str());\
+    } \
+    else {\
+      (val) = (def);\
+    }\
+  } while(0)
+
+#define SETSTR(args,val,opt,def) \
+  do {\
+		if (CONTAINS(args, opt)) {\
+			strcpy(val, args[opt].c_str());\
+		} \
+		else {\
+			strcpy(val, def);\
+		}\
+	} while(0)
+
+#define SETBOOL(args,val,opt,def) \
+  do {\
+    if (CONTAINS(args,opt)) {\
+      (val) = 1;\
+    } \
+    else {\
+      (val) = (def);\
+    }\
+  } while(0)
+
+typedef std::map<std::string, std::string> Args;
+
+
+int64_t now();
+int resolve(const char* hostname, int port, struct sockaddr_in *addr);
+int readn(int fd, char* buf, int n);
+int readline(int fd, char* buf, int n);
+int writen(int fd, char* buf, int n);
+std::map<std::string, std::string> parsecmdline(int argc, char* argv[]);
+void toUpper(char* str, int n);
---
+++ program/util/common.cc
@@ -0,0 +1,140 @@
+#include "common.h"
+
+extern int h_errno;
+
+int64_t now() {
+	int ret;
+	struct timeval now;
+	ret = gettimeofday(&now, NULL);
+	if (ret < 0) ERR_EXIT("gettimeofday");
+	return now.tv_sec * 1000000 + now.tv_usec;
+}
+
+int resolve(const char* hostname, int port, struct sockaddr_in *addr) {
+	int ret;
+	struct hostent *he;
+
+	bzero(addr, sizeof(struct sockaddr_in));
+
+	ret = inet_aton(hostname, &addr->sin_addr);
+	if (ret == 0) {
+		he = gethostbyname(hostname);
+		errno = h_errno;
+		if (!he) return -1; 
+	  addr->sin_addr= *(struct in_addr*)(he->h_addr);
+	}
+
+	addr->sin_family = AF_INET;
+	addr->sin_port = htons((short)port);
+
+	return 0;
+}
+
+int readn(int fd, char* buf, int n) {
+	int len, nread;
+
+	nread = 0;// 已经读取的字节数
+	while(nread < n) {
+		len = read(fd, buf + nread, n - nread); 
+		if (len < 0) {
+			if (errno == EINTR) continue;
+			return -1;
+		}
+		if (len == 0) break;
+		nread += len;
+	}
+	return nread;
+}
+
+int writen(int fd, char* buf, int n) {
+	int len, nwrite;
+	nwrite = 0;
+	while(nwrite < n) {
+		len = write(fd, buf + nwrite, n - nwrite);
+		if (len <= 0) {
+			if (errno == EINTR) continue;
+			return -1;
+		}
+
+		nwrite += len;
+	}
+	return nwrite;
+}
+
+
+std::map<std::string, std::string> parsecmdline(int argc, char* argv[]) {
+  int i;
+	char* cur;
+	std::map<std::string, std::string> res;
+
+	i = 1;
+	while(i < argc) {
+		cur = argv[i];
+		if (strlen(cur) < 2 || cur[0] != '-') {
+			printf("未识别的参数: %s\n", cur);
+			exit(1);
+		}
+
+		++cur;
+		if (cur[0] == '-') {
+			// 长参数
+			++cur;
+			if (cur[0] == '\0') {
+				printf("未识别的参数: %s\n", cur);
+				exit(1);
+			}
+		}
+
+		// 选项带参数
+		if (i < argc - 1 && argv[i+1][0] != '-') {
+			res[std::string(cur)] = std::string(argv[i+1]);
+			i += 2;
+		}
+		else {
+			res[std::string(cur)] = std::string("true");
+			i++;
+		}
+	}
+
+	return res;
+}
+
+int readline(int fd, char* buf, int n) {
+	int i, nr, len;
+	char c;
+
+	i = 0;
+	nr = 0;
+  while(1) {	
+		len = read(fd, &c, 1);
+		if (len == 0) return nr;
+	  else if (len < 0) {
+			if (errno == EINTR) continue;
+			return -1;
+		}	
+
+		nr += len;
+		buf[i++] = c;
+
+		if (c == '\n') {
+      break;
+		}
+	}
+	return nr;
+}
+
+void toUpper(char* str, int n) {
+	int i = 0;
+
+  while(i < n && str[i]) {
+		str[i] = toupper(str[i]);
+		++i;
+	}
+}
+
+
+
+
+
+
+
