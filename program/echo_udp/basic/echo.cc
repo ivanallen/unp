0c3005660211358aa0500c87fc9d46b0be0cf72f
allen <1007729991@qq.com>
2017-04-26 16:32:25 +0800

    add tcp and udp's template; add echo_udp

---
+++ program/echo_udp/basic/echo.cc
@@ -0,0 +1,120 @@
+#include "common.h"
+
+void doServer(int);
+void doClient(int);
+void server_routine();
+void client_routine();
+
+struct Options {
+	int isServer;
+	char hostname[32];
+	int port;
+} g_option;
+
+int main(int argc, char* argv[]) {
+	Args args = parsecmdline(argc, argv);
+	if (args.empty()){
+		printf("Usage:\n  %s [-s] [-h hostname] [-p port]\n", argv[0]);
+		return 1;
+	}
+
+
+	SETBOOL(args, g_option.isServer, "s", 0);
+	SETSTR(args, g_option.hostname, "h", "0");
+	SETINT(args, g_option.port, "p", 8000);
+
+	if (g_option.isServer) {
+		server_routine();
+	}
+	else {
+		client_routine();
+	}
+
+  return 0;
+}
+
+void server_routine() {
+	int ret, sockfd;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t cliaddrlen;
+
+	ret = resolve(g_option.hostname, g_option.port, &servaddr);
+	if (ret < 0) ERR_EXIT("resolve");
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) ERR_EXIT("socket");
+
+	ret = bind(sockfd, (struct sockaddr*)&servaddr, sizeof servaddr);
+	if (ret < 0) ERR_EXIT("bind");
+
+	doServer(sockfd);
+
+	close(sockfd);
+}
+
+void client_routine() {
+	int sockfd;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) ERR_EXIT("socket");
+
+	doClient(sockfd);
+
+	close(sockfd);
+}
+
+void doServer(int sockfd) {
+	char buf[4096];
+	int nr, nw;
+	struct sockaddr_in cliaddr;
+	socklen_t len;
+
+  while(1) {
+		len = sizeof(cliaddr);
+		nr = recvfrom(sockfd, buf, 4096, 0, (struct sockaddr*)&cliaddr, &len); 
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("recvfrom");
+		}
+		puts("...");
+	  nw = sendto(sockfd, buf, nr, 0, (struct sockaddr*)&cliaddr, len);	
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("recvfrom");
+		}
+	}
+}
+
+void doClient(int sockfd) {
+	int ret, len, nr, nw;
+	struct sockaddr_in servaddr;
+	char buf[4096];
+
+	ret = resolve(g_option.hostname, g_option.port, &servaddr);
+	if (ret < 0) ERR_EXIT("resolve");
+
+  while(1) {
+		nr = read(STDIN_FILENO, buf, 4096);
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("recvfrom");
+		}
+		else if (nr == 0) break;
+	  nw = sendto(sockfd, buf, nr, 0, (struct sockaddr*)&servaddr, sizeof(servaddr));	
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("sendto");
+		}
+		nr = recvfrom(sockfd, buf, 4096, 0, NULL, NULL); 
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("recvfrom");
+		}
+		nw = write(STDOUT_FILENO, buf, nr);
+		if (nr < 0) {
+			if (errno == EINTR) continue;
+			ERR_EXIT("sendto");
+		}
+	}
+}
+
---
+++ program/template/tcp.cc
@@ -0,0 +1,76 @@
+#include "common.h"
+
+void doServer(int);
+void doClient(int);
+void server_routine();
+void client_routine();
+
+struct Options {
+	int isServer;
+	char hostname[32];
+	int port;
+} g_option;
+
+int main(int argc, char* argv[]) {
+	Args args = parsecmdline(argc, argv);
+	if (args.empty()){
+		printf("Usage:\n  %s [-s] [-h hostname] [-p port]\n", argv[0]);
+		return 1;
+	}
+
+
+	SETBOOL(args, g_option.isServer, "s", 0);
+	SETSTR(args, g_option.hostname, "h", "0");
+	SETINT(args, g_option.port, "p", 8000);
+
+	if (g_option.isServer) {
+		server_routine();
+	}
+	else {
+		client_routine();
+	}
+
+  return 0;
+}
+
+void server_routine() {
+	int ret, listenfd, sockfd;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t cliaddrlen;
+
+	ret = resolve(g_option.hostname, g_option.port, &servaddr);
+	if (ret < 0) ERR_EXIT("resolve");
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd < 0) ERR_EXIT("socket");
+
+	ret = bind(listenfd, (struct sockaddr*)&servaddr, sizeof servaddr);
+	if (ret < 0) ERR_EXIT("bind");
+
+	ret = listen(listenfd, 5);
+	if (ret < 0) ERR_EXIT("listen");
+}
+
+void client_routine() {
+	int ret, sockfd;
+	struct sockaddr_in servaddr;
+	ret = resolve(g_option.hostname, g_option.port, &servaddr);
+	if (ret < 0) ERR_EXIT("resolve");
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) ERR_EXIT("socket");
+
+	ret = connect(sockfd, (struct sockaddr*)&servaddr, sizeof servaddr);
+	if (ret < 0) ERR_EXIT("connect");
+
+	doClient(sockfd);
+
+	close(sockfd);
+}
+
+void doServer(int sockfd) {
+}
+
+void doClient(int sockfd) {
+}
+
