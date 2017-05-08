1f98118bde7eab948b9539dadfc1e1d4a2c59c0f
allen <1007729991@qq.com>
2017-05-08 19:58:22 +0800

    modify run_client.sh

--- program/nonblockio/nbio/run_client.sh
+++ program/nonblockio/nbio/run_client.sh
@@ -5,4 +5,4 @@ length=4096
 if [ $1 ]; then
 	length=$1
 fi
-time dd if=/dev/zero bs=1024000 count=1 | ./echo -h mars $2 -l $length >/dev/null
+time dd if=/dev/zero bs=1024000 count=1 | ./echo -h mars $2 $3 -l $length >/dev/null
